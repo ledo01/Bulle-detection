224154874e6a264f82408a62e2a84935780269d5
ledo01 <ledo01@uqo.ca>
2016-06-15 15:32:04 -0400

    Clean up

--- edge.py
+++ edge.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from math import sqrt
-from skimage import feature,data, io,filters
+from skimage import feature,data, io
 from skimage.color import rgb2gray
 
 filename = os.path.join('img.png')
@@ -10,15 +9,11 @@ im_color = io.imread(filename)
 im = rgb2gray(im_color)
 im = im[550:750,860:1050]
 # im[30:145,70:125] = 0
-# Compute the Canny filter for two values of sigma
-edges1 = feature.canny(im,1)
+
+edges1 = feature.canny(im,1) # Compute the Canny filter
 y,x = np.where(edges1 == True)
 # display results
-
-
-plt.imshow(im,cmap='RdGy')
-#ax1.axis('off')
-#ax1.set_title('original image', fontsize=20)
+# plt.imshow(im,cmap='RdGy')
 
 plt.scatter(x,y,c='k')
 plt.show()
